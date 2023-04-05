4dfc8b42e72745012d215a9f97f13849323a42eb
saitek <saitek300@gmail.com>
2023-04-04 19:07:03 -0700

    changed contact form to email anchor and add. info

--- src/components/pages/Contact.js
+++ src/components/pages/Contact.js
@@ -1,81 +1,76 @@
-import React,{useState} from "react";
+import React from "react";
 
-import { validateEmail } from '../../utils/helpers';
+// import { validateEmail } from '../../utils/helpers';
 
 export default function Contact() {
   
-  const [contactName, setContactName] = useState('');
-  const [email, setEmail] = useState('');
-  const [message, setMessage] = useState('');
-  const [errorMessage, setErrorMessage] = useState('');
+  // const [contactName, setContactName] = useState('');
+  // const [email, setEmail] = useState('');
+  // const [message, setMessage] = useState('');
+  // const [errorMessage, setErrorMessage] = useState('');
 
-  const handleInput = (e) => {
+  // const handleInput = (e) => {
     
-    const { target } = e;
-    const inputType = target.name;
-    const inputValue = target.value;
+  //   const { target } = e;
+  //   const inputType = target.name;
+  //   const inputValue = target.value;
 
-    if (inputType === 'email') {
-      setEmail(inputValue);
-    } else if (inputType === 'contactName') {
-      setContactName(inputValue);
-    } else {
-      setMessage(inputValue);
-    }
-  };
+  //   if (inputType === 'email') {
+  //     setEmail(inputValue);
+  //   } else if (inputType === 'contactName') {
+  //     setContactName(inputValue);
+  //   } else {
+  //     setMessage(inputValue);
+  //   }
+  // };
 
-  const handleSubmit = (e) => {
-    e.preventDefault();
+  // const handleSubmit = (e) => {
+  //   e.preventDefault();
 
-    if (!validateEmail(email)) {
-      setErrorMessage('Email is invalid');
+  //   if (!validateEmail(email)) {
+  //     setErrorMessage('Email is invalid');
       
-      return;
+  //     return;
       
-    }
+  //   }
     
 
     
-    setContactName('');
-    setEmail('');
-    setMessage('');
-  };
+  //   setContactName('');
+  //   setEmail('');
+  //   setMessage('');
+  // };
+  const styles = {
+    h1Style:{
+        textAlign: 'center',
+        fontSize: '45px',
+        display: 'flex',
+        justifyContent: 'center',
+
+    },
+    h2Style:{
+      textAlign: 'center',
+      fontSize: '35px',
+      display: 'flex',
+      justifyContent: 'center',
+
+  },
+    textStyle: {
+        textAlign: 'center',
+        fontSize: '22px',
+        display: 'flex',
+        justifyContent: 'center',
+    },
+   
+ }
     return(
-        <div>
-            <form className="contact-form" onSubmit={handleSubmit}>
-                <h2>Contact</h2>
-                <label>Name:</label>
-                    <input
-                        type="text"
-                        placeholder="What is your name?"
-                        value={contactName}
-                        name="contactName"
-                        onChange={handleInput}
-                    ></input>
-
-                <label>Email address:</label>
-                <input
-                        type="text"
-                        placeholder="What is your email?"
-                        value={email}
-                        name="email"
-                        onChange={handleInput}
-                    ></input>
-                <label>Message:</label>
-                <input
-                        type="text"
-                        placeholder="Type your message here."
-                        value={message}
-                        name="text"
-                        onChange={handleInput}
-                    ></input>
-                     <button type="button" onClick={handleSubmit}>Submit</button>
-            </form>
-            {errorMessage && (
-        <div>
-          <p className="error-text">{errorMessage}</p>
-        </div>
-      )}
+        <div className="contact-form">
+                <h1 style={styles.h1Style}>Contact</h1>
+                <a href="mailto:saitek300@gmail.com" style={styles.textStyle}>saitek300@gmail.com</a>
+                <h2 style={styles.h2Style}>Phone #</h2>
+                <p style={styles.textStyle}>(916)475-2480</p><br></br>
+                <p style={styles.textStyle}>(*for additional contact links use the icons below)</p>
+                
         </div>
 
     )
